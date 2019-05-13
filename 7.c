c1b97484aea82d300f72cc5f4ba02fd10e8cac66
Kiran G <kiran.g@ewit.edu>
2019-05-13 07:37:30 +0530

    Update 7.c

--- 7.c
+++ 7.c
@@ -68,7 +68,9 @@ int main(int argc,char ** argv)
 printf("No of Recursive steps/Division: ");
 scanf("%d",&n);
 glutInit(&argc,argv);
-glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH); glutCreateWindow(" 3D Sierpinski gasket"); glutReshapeFunc(myReshape);
+glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
+glutCreateWindow(" 3D Sierpinski gasket");
+glutReshapeFunc(myReshape);
 glutDisplayFunc(display);
 glEnable(GL_DEPTH_TEST);
 glClearColor(1.0,1.0,1.0,0.0);
